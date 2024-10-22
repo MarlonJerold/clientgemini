269287952c07221cf98b57dabda46b98f63b5217
Marlon <63025001+MarlonJerold@users.noreply.github.com>
2024-10-22 18:57:38 -0300

    guardando bobinho

--- app.py
+++ app.py
@@ -3,31 +3,22 @@ import google.generativeai as genai
 import requests
 import os
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-os.environ["GOOGLE_API_KEY"] = "GOOGLE_API_KEY"
-api_key = os.environ["GOOGLE_API_KEY"]
+api_key = os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY não foi definida no ambiente.")
+
 genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-
-@app.route('/ask', methods=['GET'])
-def ask_question():
-    question = request.args.get('question')
-
-    if not question:
-        return jsonify({"error": "O parâmetro 'question' é obrigatório."}), 400
-
-    try:
-        # Gere o conteúdo com a pergunta
-        response = model.generate_content([question])
-        return jsonify({"response": response.text})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/summarize_posts', methods=['GET'])
 def summarize_posts():
@@ -35,7 +26,7 @@ def summarize_posts():
     
     try:
         response = requests.get(url)
-        response.raise_for_status() 
+        response.raise_for_status()
 
         data = response.json()
         post_texts = []
@@ -49,9 +40,10 @@ def summarize_posts():
             post_texts.append(f"{display_name} ({handle}): {text}")
 
         post_texts_str = " ".join(post_texts)
+
         prompt = f"Quero que você escreva um resumo em formato de jornal com base nos tópicos mais falados nos posts do Bluesky e da bolha dev. Transforme o conteúdo em um texto único, contínuo e interessante:\n\n{post_texts_str}"
 
-        summary_response = model.generate_content(prompt)
+        summary_response = model.generate_content([prompt])
 
         if summary_response and summary_response.candidates:
             text = summary_response.candidates[0].content.parts[0].text
