cd9755bb0edab3ee8a2d5fd8822475685f416e54
Marlon <63025001+MarlonJerold@users.noreply.github.com>
2024-11-04 22:31:16 -0300

    criação de keywords e question

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 
 CORS(app, resources={
     r"/summarize_posts": {"origins": ["https://www.milho.site"]},
-    r"/is_opportunity": {"origins": ["https://milharal-news.onrender.com"]}
+    r"/is_opportunity": {"origins": ["https://milharal-news.onrender.com"]},
+    r"/keywords": {"origins": ["https://milharal-news.onrender.com"]},
+    r"/question": {"origins": ["https://milharal-news.onrender.com"]}
 })
 
 logging.basicConfig(level=logging.INFO)
@@ -25,6 +27,59 @@ if not api_key:
 genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+@app.route('/keywords', methods=['GET'])
+def generate_keywords():
+    try:
+        user_input = request.args.get("text")
+
+        if not user_input:
+            return jsonify({"error": "Texto não fornecido."}), 400
+
+        prompt = (
+    f"Quais são as principais palavras-chave e tópicos específicos que capturam o contexto do texto a seguir? Responda com uma lista de 190 palavras ou termos diretamente relacionados ao universo abordado, evitando termos genéricos como 'programação' ou 'tecnologia'. Em vez disso, foque em palavras e tópicos específicos que reflitam o conteúdo de forma precisa. Por exemplo, para um texto sobre Java, liste palavras como JPA, Spring Boot, Tomcat, arquitetura REST, entre outras que representem bem o assunto.\n\n\"{user_input}\""
+            )        
+        keywords_response = model.generate_content([prompt])
+
+        if keywords_response and keywords_response.candidates:
+            response_text = keywords_response.candidates[0].content.parts[0].text.strip()
+            keywords = [keyword.strip() for keyword in response_text.split(',') if keyword.strip()]
+            return jsonify({"keywords": keywords})
+        else:
+            return jsonify({"error": "Nenhum conteúdo gerado."}), 500
+
+    except Exception as e:
+        logging.error(f"Erro inesperado: {e}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/question', methods=['GET'])
+def question():
+    try:
+    
+        post_text = request.args.get("text")
+
+        if not post_text:
+            return jsonify({"error": "Texto do post não fornecido."}), 400
+
+        prompt = f"Este texto faz sentido em relação ao objetivo pedido pelo usuário? Avalie se o conteúdo claramente corresponde ao tema especificado pelo usuário e atende ao propósito pretendido. Responda apenas com '{{\"is_relevant\": true}}' se o conteúdo do post estiver alinhado ao objetivo do usuário, ou '{{\"is_not_relevant\": false}}' se o conteúdo não estiver alinhado:\n\n\"{post_text}\""
+        verification_response = model.generate_content([prompt])
+
+        if verification_response and verification_response.candidates:
+            response_text = verification_response.candidates[0].content.parts[0].text.strip().lower()
+
+            if "true" in response_text:
+                return jsonify({"question": True})
+            elif "false" in response_text:
+                return jsonify({"question": False})
+            else:
+                return jsonify({"error": "Resposta inesperada do modelo."}), 500
+        else:
+            return jsonify({"error": "Nenhum conteúdo gerado."}), 500
+
+    except Exception as e:
+        logging.error(f"Erro inesperado: {e}")
+        return jsonify({"error": str(e)}), 500
+
 @app.route('/is_opportunity', methods=['GET'])
 def is_opportunity():
     try:
